e1cbd5fcf52a7fa1f0f21c30bb443ac61cac59b3
eogus65121 <eogus6512@gmail.com>
2021-08-25 07:00:06 +0900

    json file rename

--- src/main/java/com/ezTstock/config/SlackImpl.java
+++ src/main/java/com/ezTstock/config/SlackImpl.java
@@ -11,7 +11,7 @@ public class SlackImpl implements JsonRead {
     @Override
     public String readJ(String jKey){
         try {
-            Reader reader = new FileReader("C://ezTstock_config/test.json");
+            Reader reader = new FileReader("C://ezTstock_config/config.json");
             Gson gson = new Gson();
             JsonObject jobj = gson.fromJson(reader, JsonObject.class);
             JsonObject jobj_slack = gson.fromJson(jobj.get("slack"), JsonObject.class);
