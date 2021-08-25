5451a12aa50ebb0ff2a35b1bd2f41e7f18e3edf2
eogus65121 <eogus6512@gmail.com>
2021-08-25 17:37:27 +0900

    mapping

--- src/main/java/com/ezTstock/slack/SlackWebhookSend.java
+++ src/main/java/com/ezTstock/slack/SlackWebhookSend.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 public class SlackWebhookSend {
-    @GetMapping("/send")
+    @GetMapping("/slack/sendMessage")
     public void send(){
         RestTemplate restTemplate = new RestTemplate();
 
