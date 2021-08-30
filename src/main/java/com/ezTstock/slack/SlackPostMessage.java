74015d5594b3d5cfb5bf15ccdd57c34b7bba6d7f
eogus65121 <eogus6512@gmail.com>
2021-08-31 01:03:13 +0900

    send message

--- src/main/java/com/ezTstock/slack/SlackPostMessage.java
+++ src/main/java/com/ezTstock/slack/SlackPostMessage.java
@@ -3,6 +3,8 @@ package com.ezTstock.slack;
 import com.ezTstock.config.SlackImpl;
 import com.slack.api.Slack;
 import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import com.slack.api.model.Message;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
@@ -11,7 +13,7 @@ public class SlackPostMessage {
     /**
      * Post a message to a channel your app is in using ID and message text
      */
-    static void slackPostMessage(String text) {
+    /*static void slackPostMessage(String text) {
         // you can get this instance via ctx.client() in a Bolt app
         SlackImpl slack_json = new SlackImpl();
         String token = slack_json.readJ("token");
@@ -33,10 +35,33 @@ public class SlackPostMessage {
         } catch (IOException | SlackApiException e) {
             logger.error("error: {}", e.getMessage(), e);
         }
+    }*/
+
+    static void sendMessage(String text){
+        SlackImpl slack_json = new SlackImpl();
+        String token = slack_json.readJ("token");
+        String channel_id = slack_json.readJ("channel");
+        try {
+            ChatPostMessageResponse response = Slack.getInstance().methods(token).chatPostMessage(req -> req
+                    .channel(channel_id)
+                    .text(text));
+            if (response.isOk()) {
+                Message postedMessage = response.getMessage();
+            } else {
+                String errorCode = response.getError(); // e.g., "invalid_auth", "channel_not_found"
+            }
+        } catch (SlackApiException requestFailure) {
+            // Slack API responded with unsuccessful status code (= not 20x)
+        } catch (IOException connectivityIssue) {
+            // Throwing this exception indicates your app or Slack servers had a connectivity issue.
+        }
     }
-/*
+
+    /* 출력용
     public static void main(String[] args) throws Exception {
-        slackPostMessage("post message test");
+        //slackPostMessage("post message test");
+        sendMessage("new test message");
     }
- */
+     */
+
 }
