a373a89342c2ef7e15e747347badba2e62dee9af
tsKIM <tay97kim@daegu.ac.kr>
2021-09-02 02:51:33 +0900

    Add stock subject information crawling code

--- src/main/java/com/ezTstock/parsing/CurrentStockInfo.java
+++ src/main/java/com/ezTstock/parsing/CurrentStockInfo.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 
 public class CurrentStockInfo {
-    static void javaParsing(String subject) throws IOException {
+    static String javaParsing(String subject) throws IOException {
         Document getCode = Jsoup.connect("https://www.ktb.co.kr/trading/popup/itemPop.jspx")
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36")
                 .header("Accept-Language", "ko")
@@ -18,18 +18,40 @@ public class CurrentStockInfo {
         Elements elementC = getCode.select("select > option");
 
         String eleToStr = elementC.toString();
-        String[] tmp = eleToStr.split(subject);
-        String subject_code = tmp[0].substring(tmp[0].length()-8,tmp[0].length()-2);
+        String[] tmp1 = eleToStr.split(subject);
+        String subject_code = tmp1[0].substring(tmp1[0].length()-8,tmp1[0].length()-2);
 
-        Document searchDoc = Jsoup.connect("https://finance.naver.com/search/searchList.nhn?query="+subject_code).get();
+        Document searchDoc = Jsoup.connect("https://finance.naver.com/item/sise.nhn?code="+subject_code).get(); // 네이버> http://www.paxnet.co.kr/stock/analysis/main?abbrSymbol=
 
-        //Elements elementImg = searchDoc.select("div >");
+        String returnVal = "[ "+subject+" 정보 ]\n";
+        returnVal += "-현재가: "+searchDoc.getElementById("_nowVal").text()+"\n"; //현재가 추가
+        String diff = searchDoc.getElementById("_diff").text(); // 증감치 추가
+        diff = changePM(diff);
+        returnVal += "-증감치: "+diff+"\n";
+        returnVal += "-증감률: "+searchDoc.getElementById("_rate").text()+"\n"; // 증감률 추가
+        String[] tmp2 = searchDoc.toString().split("전일가");
+        String[] tmp3 = tmp2[1].split("거래량");
+        String[] tmp4 = tmp3[0].split("\n"); // tmp4[0] => 전일가
+        String yesterday = tmp4[0];
+        yesterday = yesterday.substring(1,yesterday.length());
+        returnVal += "-전일가: "+yesterday+"\n"; // 전일가 추가
+        returnVal += "-거래량: "+searchDoc.getElementById("_quant").text()+"\n"; // 거래량 추가
+        returnVal += "-거래대금: "+searchDoc.getElementById("_amount").text()+"백만\n"; // 거래대금 추가
+        returnVal += "http://cichart.paxnet.co.kr/pax/chart/candleChart/V201716/paxCandleChartV201716Daily.jsp?abbrSymbol="+subject_code+"\n";
 
-        System.out.println(searchDoc);
+        return returnVal;
+    }
+
+    public static String changePM(String diff){ // 상승과 하락으로 표시된 증감치에 대한 설명을 +와 -로 대치
+        if(diff.startsWith("하락 "))
+            diff = diff.replace("하락 ", "-");
+        else if(diff.startsWith("상승 "))
+            diff = diff.replace("상승 ", "+");
+        return diff;
     }
       //출력문
     public static void main(String[] args) throws IOException {
-        javaParsing("LG전자");
+        System.out.println(javaParsing("카카오"));
     }
 
 }
