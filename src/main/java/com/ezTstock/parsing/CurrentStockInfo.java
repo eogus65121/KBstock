affc485e26f9787f55380e019f035598821d2c15
eogus65121 <eogus6512@gmail.com>
2021-09-01 23:40:07 +0900

    현재 주식 동향

--- src/main/java/com/ezTstock/parsing/CurrentStockInfo.java
+++ src/main/java/com/ezTstock/parsing/CurrentStockInfo.java
@@ -1,4 +1,4 @@
-package com.ezTstock.slack;
+package com.ezTstock.parsing;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -7,8 +7,8 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 
-public class Test {
-    static void javaParsing(String url) throws IOException {
+public class CurrentStockInfo {
+    static void javaParsing(String subject) throws IOException {
         Document getCode = Jsoup.connect("https://www.ktb.co.kr/trading/popup/itemPop.jspx")
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36")
                 .header("Accept-Language", "ko")
@@ -18,16 +18,16 @@ public class Test {
         Elements elementC = getCode.select("select > option");
 
         String eleToStr = elementC.toString();
-        String[] tmp = eleToStr.split(url);
-        String code = tmp[0].substring(tmp[0].length()-8,tmp[0].length()-2);
+        String[] tmp = eleToStr.split(subject);
+        String subject_code = tmp[0].substring(tmp[0].length()-8,tmp[0].length()-2);
 
-        Document searchDoc = Jsoup.connect("https://finance.naver.com/search/searchList.nhn?query="+code).get();
+        Document searchDoc = Jsoup.connect("https://finance.naver.com/search/searchList.nhn?query="+subject_code).get();
 
-        Elements elementImg = searchDoc.select("div >");
+        //Elements elementImg = searchDoc.select("div >");
 
         System.out.println(searchDoc);
     }
-
+      //출력문
     public static void main(String[] args) throws IOException {
         javaParsing("LG전자");
     }
