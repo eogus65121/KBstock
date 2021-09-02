fb7fbc42bfccebc45baf6d3fc55fe1c9115eae88
eogus65121 <eogus6512@gmail.com>
2021-09-03 01:17:01 +0900

    코드 수정 add simple json maven

--- src/main/java/com/ezTstock/config/DBImpl.java
+++ src/main/java/com/ezTstock/config/DBImpl.java
@@ -11,7 +11,7 @@ public class DBImpl implements JsonRead{
     @Override
     public String readJ(String jKey){
         try {
-            Reader reader = new FileReader("");
+            Reader reader = new FileReader("C://ezTstock_config/config.json");
             Gson gson = new Gson();
             JsonObject jobj = gson.fromJson(reader, JsonObject.class);
             JsonObject jobj_db = gson.fromJson(jobj.get("stock_db"), JsonObject.class);
