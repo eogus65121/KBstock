2977ceb5a605e9ba05a062c6f7bb0e95831d57ef
eogus65121 <eogus6512@gmail.com>
2021-09-06 01:44:12 +0900

    command news update

--- src/main/java/com/ezTstock/slack/slashController/CommandNews.java
+++ src/main/java/com/ezTstock/slack/slashController/CommandNews.java
@@ -1,10 +1,10 @@
 package com.ezTstock.slack.slashController;
 
 import com.ezTstock.parsing.StockNews;
-import com.ezTstock.slack.dto.SlashCommandRequest;
 import com.ezTstock.slack.function.SlackSendMessage;
-import lombok.extern.slf4j.Slf4j;
+import com.ezTstock.slack.dto.SlashCommandRequest;
 import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +15,22 @@ import java.io.IOException;
 @RestController
 @RequestMapping
 public class CommandNews {
+    SlackSendMessage sendMessage = new SlackSendMessage();
+    StockNews stockNews = new StockNews();
+
     @PostMapping(value = "/slack/command/news", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public void commandNews(SlashCommandRequest dataPayload){
         log.info("Request 'POST /slack/command/news' request: {}", dataPayload);
-        String subject = dataPayload.toString().split("text=")[1].split(",")[0].replaceAll(" ", "");
-        SlackSendMessage sendMessage = new SlackSendMessage();
-        StockNews stockNews = new StockNews();
-
-        try{
-            String[] news = stockNews.javaParsingNews(subject);
-            sendMessage.slackSendMessage(news);
-        }catch(IOException e){
-            e.printStackTrace();
+        String subject = dataPayload.getText().replaceAll(" ", "");
+        if(subject.equals("")){
+            sendMessage.slackSendMessage("종목을 입력해주세요.");
+        }else{
+            try{
+                String[] news_text = stockNews.javaParsingNews(subject);
+                sendMessage.slackSendMessage(news_text);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         }
-
     }
 }
