1103930c053c0e8ac356f62f1784fa3440b90b92
eogus65121 <eogus6512@gmail.com>
2021-09-06 17:24:22 +0900

    userProfileMapper update

--- src/main/java/com/ezTstock/stock_db/mapper/userProfileMapper.java
+++ src/main/java/com/ezTstock/stock_db/mapper/userProfileMapper.java
@@ -1,17 +1,20 @@
 package com.ezTstock.stock_db.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.springframework.jmx.export.annotation.ManagedOperationParameter;
 
 @Mapper
 public interface userProfileMapper {
 
-    @Select("select name from user where name=#{name}")
+    @Select("select name from user where name=#{name}") // user select
     String getUserName(@Param("name") String name);
 
-    @Insert("insert into user values(#{name}, #{requirement})")
+    @Insert("insert into user values(#{name}, #{requirement})") //user insert
     void insertUserProfile(@Param("name") String name, @Param("requirement") String requirement);
 
+    @Delete("delete from user where name=#{name}") // user delete
+    void deleteUserProfile(@Param("name") String name);
+
+    @Update("update user set requirement=#{requirement} where name=#{name}")  // user on off
+    void updateUserRequirement(@Param("requirement") String requirement, @Param("name") String name);
 }
