65c0934929d7cade6d65d1299e418124b84e2b82
eogus65121 <eogus6512@gmail.com>
2021-09-06 17:25:00 +0900

    create varianceValue about db variance value table

---
+++ src/main/java/com/ezTstock/stock_db/model/varianceValue.java
@@ -0,0 +1,19 @@
+package com.ezTstock.stock_db.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class varianceValue {
+    private String subject_name;
+    private String user_name;
+    private float value;
+
+    public varianceValue(String subject_name, String user_name, float value){
+        super();
+        this.subject_name = subject_name;
+        this.user_name = user_name;
+        this.value = value;
+    }
+}
