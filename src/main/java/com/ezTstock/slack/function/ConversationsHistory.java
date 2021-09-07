d98d37c7c3ee1426f5018419b7590dad7b0e1ef2
eogus65121 <eogus6512@gmail.com>
2021-09-08 04:20:41 +0900

    conversation history update

--- src/main/java/com/ezTstock/slack/function/ConversationsHistory.java
+++ src/main/java/com/ezTstock/slack/function/ConversationsHistory.java
@@ -16,24 +16,26 @@ public class ConversationsHistory {
 
     static Optional<List<Message>> conversationHistory = Optional.empty();
 
-    public static String fetchHistory() {
+    public String[] fetchHistory() {
         SlackImpl slack_json = new SlackImpl();
         String bot_token = slack_json.readJ("bot_token");
         String channel = slack_json.readJ("channel");
+        String [] history = new String[2];
         var client = Slack.getInstance().methods(bot_token);
         var logger = LoggerFactory.getLogger("my-awesome-slack-app");
         try {
             var result = client.conversationsHistory(r -> r
                     .channel(channel)
-                    .limit(1)
+                    .limit(2)
             );
             conversationHistory = Optional.ofNullable(result.getMessages());
-            String history_text = conversationHistory.toString().split("text=")[1].split(",")[0];
+            history[0] = conversationHistory.toString().split("text=")[3].split(",")[0];
+            history[1] = conversationHistory.toString().split("text=")[1].split(",")[0];
             logger.info("{} messages found in {}", conversationHistory.orElse(emptyList()).size(), channel);
-            return history_text;
+            return history;
         } catch (IOException | SlackApiException e) {
             logger.error("error: {}", e.getMessage(), e);
         }
-        return "";
+        return history;
     }
 }
